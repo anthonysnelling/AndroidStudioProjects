c816b53a41b2ced713dd786e7e3fc89407b762c4
Anthony Snelling <anthony.snelling@live.com>
2021-04-05 17:01:00 -0400

    added some java practice

---
+++ javaplayground/LearnJava/src/main/java/com/anthonysnelling/learnjava/Microphone.java
@@ -0,0 +1,24 @@
+package com.anthonysnelling.learnjava;
+
+public class Microphone {
+//    instance variables/ properties/ fields
+    String name;
+    String color;
+    int model;
+
+    //Actions/ Methods
+    public void  turnOff(){
+        System.out.println(this.name + " Turn off");
+    }
+    public void  turnOn(){
+        System.out.println(this.name + " Turn on");
+    }
+    public void  setVolume(){
+        System.out.println(this.name + " Setting volume");
+    }
+
+    public String showDescription(){
+        return "Microphone name: " + this.name + ", with color: " + this.color + ", and its model " + this.model;
+    }
+
+}
--- javaplayground/LearnJava/src/main/java/com/anthonysnelling/learnjava/MyClass.java
+++ javaplayground/LearnJava/src/main/java/com/anthonysnelling/learnjava/MyClass.java
@@ -2,7 +2,30 @@ package com.anthonysnelling.learnjava;
 
 public class MyClass {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Another Day!");
+        Microphone microphone = new Microphone(); //instantiating our object
+        microphone.color = "Blue";
+        microphone.name = "Blue Yeti";
+        microphone.model = 133543;
+
+        Microphone ghMic = new Microphone();
+        ghMic.model = 465;
+        ghMic.name = "GHZB";
+        ghMic.color = "Red";
+        System.out.println(microphone.name + " " + microphone.color + " " + microphone.model);
+        System.out.println(ghMic.name);
+
+        Microphone shure = new Microphone();
+        shure.model = 123123;
+        shure.name = "Shure MX";
+        shure.color = "Black";
+        System.out.println(shure.name);
+
+        microphone.turnOn();
+        microphone.setVolume();
+        microphone.turnOff();
+        System.out.println(ghMic.showDescription());
+        ghMic.turnOn();
+        ghMic.setVolume();
+        ghMic.turnOff();
     }
 }
